a33a7f490b467391d5f9d0f4eb8aacdfb1fe5838
Sleeba Paul <sleebapaul@gmail.com>
2020-10-17 15:00:01 +0000

    Add line empty check

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,27 +1,29 @@
 import sys
 
+
 def hw():
     afinnfile = None
     outputfile = None
     with open("AFINN-111.txt") as f:
         afinnfile = f.readlines()
 
-    scores = {} # initialize an empty dictionary
+    scores = {}  # initialize an empty dictionary
     for line in afinnfile:
-        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        # The file is tab-delimited. "\t" means "tab character"
+        term, score = line.split("\t")
         scores[term] = int(score)  # Convert the score to an integer.
 
     with open("output.txt") as f:
         outputfile = f.readlines()
-    
-    for line in outputfile:
-        words = line.split()
-        sentiment = 0 
-        for word in words:
-            if word.lower() in scores.keys():
-                sentiment += scores[word.lower()]
-        print(sentiment)
 
+    for line in outputfile:
+        if line.strip():
+            words = line.strip().split()
+            sentiment = 0
+            for word in words:
+                if word.lower() in scores.keys():
+                    sentiment += scores[word.lower()]
+            print(sentiment)
 
 def lines(fp):
     print(str(len(fp.readlines())))
@@ -32,6 +34,7 @@ def main():
     hw()
     lines(sent_file)
     lines(tweet_file)
+
 
 if __name__ == '__main__':
     main()
